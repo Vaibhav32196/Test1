afc1c97b0bd7188f7ba92ab0f44d7d9093ba61af
Vaibhav <Vaibhav@LAPTOP-O631N447>
2022-10-05 12:52:38 +0530

    Commit 1

---
+++ src/Sample1.java
@@ -0,0 +1,16 @@
+
+public class Sample1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Good Morning");
+		
+		M1();
+		
+	}
+
+	public static void M1 () 
+	{
+		System.out.println("Hii");
+	}
+}
